304740f5de46592f86402253f828bd4985817532
IlanP <ilan.petiot@icloud.com>
2022-11-25 16:02:55 +0100

    ⚙️ - Add script to display date / time dynamically
    Add also Jquery and change title

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!doctype html>
 <html lang="fr">
 <head>
-    <title>My Page</title>
+    <meta charset="utf-8">
+    <title>Octolo Exo</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+            integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -19,5 +22,39 @@
         </div>
     </div>
 </div>
+
+<script>
+
+    function updateJson() {
+        $.ajax({
+            url: 'back.php',
+        });
+    }
+
+    function displayDate() {
+        $.ajax({
+            url: 'data.json',
+            dataType: 'json',
+            success: function (data) {
+                var date = data.data.date;
+                var time = data.data.time;
+                var timezone = data.data.timezone;
+                $('#date').html(date);
+                $('#time').html(time);
+                $('#timezone').html(timezone);
+            }
+        });
+    }
+
+    $(document).ready(function () {
+        updateJson();
+        displayDate();
+        setInterval(function () {
+            updateJson();
+            displayDate();
+        }, 100);
+    });
+
+</script>
 </body>
 </html>
